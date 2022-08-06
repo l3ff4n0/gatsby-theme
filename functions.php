8613e1a0dd05d0c399aa4a51765a34aaa79b5285
l3ff4n0 <frasson.stefano@gmail.com>
2022-08-06 18:59:58 +0200

    First configuration of WordPress theme

---
+++ functions.php
@@ -0,0 +1,12 @@
+<?php
+// Register your menus
+function gatsby_theme_custom_menus() {
+    $locations = array(
+        'primary'   => __( 'Primary Menu', 'gastby-menu' ),
+        'footer'  => __( 'Footer Menu', 'gastby-menu' ),
+    );
+    register_nav_menus( $locations );
+ }
+
+// Hook them into the theme-'init' action
+add_action( 'init', 'gatsby_theme_custom_menus' );
